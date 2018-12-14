377d201da4ca0a5d0482aa3fd6ee4987ac4df13c
Mobi <1228380609@qq.com>
2018-12-14 09:32:44 +0800

    change

--- src/page/index/Home/Header/Header.jsx
+++ src/page/index/Home/Header/Header.jsx
@@ -6,6 +6,7 @@ class Header extends React.Component{
     render(){
         return(
             <div className="header">
+                <div>ddasdsadsaa</div>
                 <img src="https://m.mmm920.com/upload/banner/20181207181519030dd.jpg"/>
             </div>
         )
